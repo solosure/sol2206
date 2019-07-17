5c03677f6982278acdd215a6714067240a1c270e
solosure <solosure@163.com>
2019-07-17 15:32:25 +0800

    去掉decltype(auto)

--- sol/property.hpp
+++ sol/property.hpp
@@ -49,43 +49,43 @@ namespace sol {
 
 	namespace property_detail {
 		template <typename R, typename W>
-		inline decltype(auto) property(std::true_type, R&& read, W&& write) {
+		inline auto property(std::true_type, R&& read, W&& write) -> decltype(property_wrapper<tao::decay_t<R>, tao::decay_t<W>>(std::forward<R>(read), std::forward<W>(write))) {
 			return property_wrapper<tao::decay_t<R>, tao::decay_t<W>>(std::forward<R>(read), std::forward<W>(write));
 		}
 		template <typename W, typename R>
-		inline decltype(auto) property(std::false_type, W&& write, R&& read) {
+		inline auto property(std::false_type, W&& write, R&& read) -> decltype(property_wrapper<tao::decay_t<R>, tao::decay_t<W>>(std::forward<R>(read), std::forward<W>(write))) {
 			return property_wrapper<tao::decay_t<R>, tao::decay_t<W>>(std::forward<R>(read), std::forward<W>(write));
 		}
 		template <typename R>
-		inline decltype(auto) property(std::true_type, R&& read) {
+		inline auto property(std::true_type, R&& read) -> decltype(property_wrapper<tao::decay_t<R>, void>(std::forward<R>(read), no_prop())) {
 			return property_wrapper<tao::decay_t<R>, void>(std::forward<R>(read), no_prop());
 		}
 		template <typename W>
-		inline decltype(auto) property(std::false_type, W&& write) {
+		inline auto property(std::false_type, W&& write) -> decltype(property_wrapper<void, tao::decay_t<W>>(no_prop(), std::forward<W>(write))) {
 			return property_wrapper<void, tao::decay_t<W>>(no_prop(), std::forward<W>(write));
 		}
 	} // namespace property_detail
 
 	template <typename F, typename G>
-	inline decltype(auto) property(F&& f, G&& g) {
+	inline auto property(F&& f, G&& g) -> decltype(property_detail::property(meta::boolean<(lua_bind_traits<meta::unqualified_t<F>>::free_arity < lua_bind_traits<meta::unqualified_t<G>>::free_arity)>(), std::forward<F>(f), std::forward<G>(g))) {
 		typedef lua_bind_traits<meta::unqualified_t<F>> left_traits;
 		typedef lua_bind_traits<meta::unqualified_t<G>> right_traits;
 		return property_detail::property(meta::boolean<(left_traits::free_arity < right_traits::free_arity)>(), std::forward<F>(f), std::forward<G>(g));
 	}
 
 	template <typename F>
-	inline decltype(auto) property(F&& f) {
+	inline auto property(F&& f) -> decltype(property_detail::property(meta::boolean<(lua_bind_traits<meta::unqualified_t<F>>::free_arity < 2)>(), std::forward<F>(f))) {
 		typedef lua_bind_traits<meta::unqualified_t<F>> left_traits;
 		return property_detail::property(meta::boolean<(left_traits::free_arity < 2)>(), std::forward<F>(f));
 	}
 
 	template <typename F>
-	inline decltype(auto) readonly_property(F&& f) {
+	inline auto readonly_property(F&& f) -> decltype(property_detail::property(std::true_type(), std::forward<F>(f))) {
 		return property_detail::property(std::true_type(), std::forward<F>(f));
 	}
 
 	template <typename F>
-	inline decltype(auto) writeonly_property(F&& f) {
+	inline auto writeonly_property(F&& f) -> decltype(property_detail::property(std::false_type(), std::forward<F>(f))) {
 		return property_detail::property(std::false_type(), std::forward<F>(f));
 	}
 
